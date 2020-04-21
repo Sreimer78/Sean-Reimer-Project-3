34d6afd4200eab9516231653c807a4d28bbfd8a2
Sean Reimer <sreimer@hawk.iit.edu>
2020-04-20 23:20:09 -0500

    Update script.js
    I am adding javascript to my google maps

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
-  function initMap(){
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
+function init(){
+	var el = document.getElementById('canvas');
+	var myLocation = new google.maps.LatLng(41.880858, -87.674069);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	};
+
+	var myMap = new google.maps.Map(el, mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: myLocation,
+		map: myMap,
+		animation: google.maps.Animation.BOUNCE,
+		icon: 'images/Marshmello.jpg'
+	});
+
+	var contentString = '<h1>IIT Perlstein Hall</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate odit optio, voluptatem placeat odio dignissimos illo magnam esse asperiores voluptas at iure vero eum, nemo aperiam? Ipsam, atque nobis rem.</p>';
+
+	var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  	});
+
+	google.maps.event.addListener(marker, 'mouseover', function() {
+    	infowindow.open(myMap, marker);
+  	});
+
+
 }
+
+google.maps.event.addDomListener(window, 'load', init);
